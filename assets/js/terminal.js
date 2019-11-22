145fe6859137d9bba60d2b9bdadc45ab1d28f26e
Lewis Tansley <officialinsanityproject@gmail.com>
2019-11-22 14:00:46 +0000

    Terminal Update

--- assets/js/terminal.js
+++ assets/js/terminal.js
@@ -10,23 +10,36 @@ $(function() {
     $('#input').on('keydown',function search(e) {
           if(e.keyCode == 13) {
   
-        var span_Text = document.getElementById("path").innerText; // Get path
+        var pathText = document.getElementById("path").innerText; // Get path
+        var pointerText = document.getElementById("pointer").innerText; // Get pointer
+        console.log(pathText+' '+pointerText);
+
+
         valFunc = null;
   
-          $('#history').append(span_Text+$(this).val()+'<br/>'); // append history
+          $('#history').append(pathText+pointerText+$(this).val()+'<br/>'); // append history
         
         if($(this).val().substring(0, 3) === 'cd '){
-          $('#path').html('MAINFRAME:\\\\SUPERUSER\\'+$(this).val().substring(3)+'&nbsp;>&nbsp;'); // change path
+          $('#path').html(pathText+'\\'+$(this).val().substring(3)); // change path
           valFunc = 1;
         }
   
         if($(this).val().substring(0) === 'root'){ 
-          $('#path').html('MAINFRAME:\\\\SUPERUSER'+'&nbsp;>&nbsp;'); // go to root
+          $('#path').html('MAINFRAME:\\\\SUPERUSER'); // go to root
           valFunc = 1;
         }
         
         if($(this).val().substring(0) === 'clear'){
             document.getElementById("history").innerHTML = "";
+            valFunc = 1;
+        }
+
+        if($(this).val().substring(0) === 'run'){
+
+            if(pathText === 'MAINFRAME:\\\\SUPERUSER\\D\\21031984\\N\\01.img'){
+            window.open("D21031984N01");
+            }
+
             valFunc = 1;
         }
   
